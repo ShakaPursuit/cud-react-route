8381bf6afc39bdaebb913215e9db05b24d5af4c2
ShakaPursuit <shakalivngstone@pursuit.org>
2023-08-11 11:57:44 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+
+import './App.css'
+import {BrowserRouter as Router} from 'react-router-dom'
+function App() {
+ 
+
+  return (
+    <>
+    <Router>
+ 
+       <h1>screen views</h1>
+    
+
+       </Router>
+    
+    </>
+  );
+}
+
+export default App
